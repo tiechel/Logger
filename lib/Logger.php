9c0e2e9cbaa7ee31fdc4622edd883ddeedab43ae
tiechel <alicetype.luna@gmail.com>
2017-07-21 10:41:40 +0900

    Update Logger.php
    デフォルト値の設定
    変数、配列、オブジェクトをそのまま渡せるように変更

--- lib/Logger.php
+++ lib/Logger.php
@@ -1,120 +1,132 @@
 <?php
 
 class Logger {
-    const ALL   = 9999;
-    const DEBUG = 600;
-    const TRACE = 500;
-    const INFO  = 400;
-    const WARN  = 300;
-    const ERROR = 200;
-    const FATAL = 100;
-    const OFF   = -1;
+    const DEBUG  = 100;
+    const TRACE  = 150;
+    const INFO   = 200;
+    const NOTICE = 250;
+    const WARN   = 300;
+    const ERROR  = 400;
+    const FATAL  = 500;
 
-    private static $level_lsit = array('ALL', 'DEBUG', 'TRACE', 'INFO', 'WARN', 'ERROR', 'FATAL', 'OFF');
-    private static $level  = "ALL";    // ログレベル
-    private static $append = true;     // 追記モード
+    protected static $levels = array(
+        100 => 'DEBUG',
+        150 => 'TRACE',
+        200 => 'INFO',
+        250 => 'NOTICE',
+        300 => 'WARN',
+        400 => 'ERROR',
+        500 => 'FATAL'
+    );
+    private static $level  = 100;       // ログレベル
+    private static $append = true;      // 追記モード
 
-    // ログフォルダ、ログファイル名
-    private static $filepath;
-
-    // ファイルハンドラ
-    private static $fp;
+    private static $filepath = null;    // ログフォルダ、ログファイル名
+    private static $fp = null;          // ファイルハンドラ
 
     /******************************
         ログファイル出力先とログレベルの設定
     ******************************/
-    public static function setting($filepath, $level){
-        self::$filepath = $filepath;
-        self::$level = in_array(ucwords($level), self::$level_lsit) ? 'ALL' : ucwords($level);
+    public static function setting($filepath = null, $level = null){
+        if (!is_null($filepath)) self::$filepath = $filepath;
+        if (!is_null($level)) self::$level = $level;
     }
 
     /******************************
         ログファイルを開く
     ******************************/
-    private static function openFile(){
+    private static function open_file(){
+        // フォルダが無ければ作成する
         if (!is_file(self::$filepath)) {
             $dir = dirname(self::$filepath);
             if (!is_dir($dir)) {
                 $success = mkdir($dir, 0777, true);
                 if ($succcess === false) {
-                    self::sendWarn("Failed creating target directory [$dir].");
+                    self::send_error("Failed to create a directory [{$dir}].");
                     return false;
                 }
             }
         }
 
+        // ファイルを開きポインタを後ろに移動する
         $mode = self::$append ? 'a' : 'w';
-        self::$fp = fopen(self::$filepath, $mode);
-        if (self::$fp === false) {
-            self::sendWarn("Failed to open a log file.");
-            self::$fp = null;
+        $fp = fopen(self::$filepath, $mode);
+        if ($fp === false) {
+            self::send_error("Failed to open.");
+            $fp = null;
             return false;
         }
-        fseek(self::$fp, 0, SEEK_END);
+        fseek($fp, 0, SEEK_END);
+        return $fp;
     }
 
     /******************************
         書き込み
     ******************************/
     protected static function write($str){
-        if (!isset(self::$fp)) {
-            if(self::openFile() === false) return;
+        if (SERVER === DEVELOP) $str = mb_convert_encoding($str, 'sjis-win');
+
+        if (self::$filepath === null) {
+            error_log($str);
+            return;
         }
-        if (fwrite(self::$fp, $str) === false) {
-            self::sendWarn("Failed to write. ");
-        };
+        if (($fp = self::open_file()) === false) {
+            return;
+        }
+        if (fwrite($fp, $str.PHP_EOL) === false) {
+            self::send_error("Failed to write.");
+        }
     }
 
     /******************************
         ログ出力
     ******************************/
-    public static function fatal($str){ if(self::getLevel() > self::FATAL) self::write(self::logformat("FATAL", $str)); }
-    public static function error($str){ if(self::getLevel() > self::ERROR) self::write(self::logformat("ERROR", $str)); }
-    public static function warn($str) { if(self::getLevel() > self::WARN ) self::write(self::logformat("WARN ", $str)); }
-    public static function info($str) { if(self::getLevel() > self::INFO ) self::write(self::logformat("INFO ", $str)); }
-    public static function trace($str){ if(self::getLevel() > self::TRACE) self::write(self::logformat("TRACE", $str)); }
-    public static function debug($str){ if(self::getLevel() > self::DEBUG) self::write(self::logformat("DEBUG", $str)); }
+    protected static function log($level, $value) {
+        if (self::$level > $level) return;
+
+        // 配列とオブジェクトはStringに変換
+        if (is_object($value)) {
+            $rc = new ReflectionClass(get_class($value));
+            if ($rc->hasMethod('__toString')) {
+                $value = (string)$value;
+            } else {
+                $value = var_export($value, true);
+            }
+        } else if (is_array($value)) {
+            $value = var_export($value, true);
+        }
+
+        $level_str = self::$levels[$level];
+        self::write(self::logformat($level_str, $value));
+    }
+
+    public static function fatal ($value) { self::log(self::FATAL,  $value); }
+    public static function error ($value) { self::log(self::ERROR,  $value); }
+    public static function warn  ($value) { self::log(self::WARN,   $value); }
+    public static function notice($value) { self::log(self::NOTICE, $value); }
+    public static function info  ($value) { self::log(self::INFO,   $value); }
+    public static function trace ($value) { self::log(self::TRACE,  $value); }
+    public static function debug ($value) { self::log(self::DEBUG,  $value); }
 
     /******************************
-        出力レベル設定値取得
+        ログの書式設定
     ******************************/
-    protected static function getLevel() {
-        switch (strtoupper(self::$level)) {
-            case "ALL"  : return self::ALL;
-            case "DEBUG": return self::DEBUG;
-            case "TRACE": return self::TRACE;
-            case "INFO" : return self::INFO;
-            case "WARN" : return self::WARN;
-            case "ERROR": return self::ERROR;
-            case "FATAL": return self::FATAL;
-            case "OFF"  : return self::OFF;
-            default: return self::ALL;
-        }
+    public static function logformat($level_str, $str) {
+        $str = rtrim($str, "\n");
+        $user = Auth::login_user();
+        $userid = $user === null ? '' : $user->userid;
+
+        $logstr = sprintf("%s [%s]%s %s", date("Y-m-d H:i:s"), $level_str, '('.$userid.')', $str);
+        return $logstr;
     }
+
     /******************************
         WARNINGを飛ばす
     ******************************/
-    protected static function sendWarn($message) {
-        throw new LoggerException2("LogError: $message");
-    }
-
-    /******************************
-        ログフォーマット
-    ******************************/
-    public static function logformat($level, $str) {
-        $str = rtrim($str, "\n");
-        $logstr = sprintf("%s [%s] %s\n", date("Y-m-d H:i:s"), $level, $str);
-        return $logstr;
+    protected static function send_error($message) {
+        throw new LoggerException("LogError: $message");
     }
 }
 
-class LoggerException extends Exception{
+class LoggerException extends RuntimeException{
 }
-
-Logger::setting('path/to/logfile.log', 'TRACE');
-Logger::trace("warning");
-Logger::debug("warning");
-Logger::info("warning");
-Logger::warn("warning");
-Logger::errror("warning");
-Logger::fatal("warning");
